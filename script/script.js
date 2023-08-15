f843de2ab16c51e893ba897bd891baa0912b4b0b
Mou786Aha <86787369+Mou786Aha@users.noreply.github.com>
2023-08-15 10:34:50 +0530

    Update script.js

--- script/script.js
+++ script/script.js
@@ -119,7 +119,7 @@ html.style.height = `${visualViewport.height}px`;
 var body = document.getElementById("body");
 body.style.marginTop = `${(visualViewport.height - 510)/2}px`;
 body.style.display = "inline-block";
-body.style.width = "510px";
-body.style.marginLeft = `${(visualViewport.width - 510)/2}px`;
+body.style.width = "500px";
+body.style.marginLeft = `${(visualViewport.width - 500)/2}px`;
 
 var battery_icon = document.getElementById("battery-icon");
